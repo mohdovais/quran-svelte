84f0e4a1f32ec94c101bb250c40ad0e2ee60ac76
Mohd Ovais <ovais.m@gmail.com>
2019-12-17 03:28:39 +0100

    ios 11 support

--- src/component/App.svelte
+++ src/component/App.svelte
@@ -13,24 +13,41 @@
             <Sura {...sura} />
         {/each}
     </main>
+    <!--
     {#if $store.page.length > 0}
     <footer>
         <Nav store={store} />
     </footer>
     {/if}
+    -->
 </section>
 
 <style>
-    section{
-        padding-top: 50px;
+    main {
+        padding: 0 0 75px 0;
+        padding-bottom: calc(75px + env(safe-area-inset-bottom));
     }
     .topbar {
         background: gold;
-        border-bottom: 1px solid goldenrod;
+        border-top: 1px solid goldenrod;
         position: fixed;
-        top: 0;
+        bottom: 0;
         left: 0;
         right: 0;
         z-index: 1;
+    }
+
+    @media (min-width: 1025px){
+        main {
+            padding: 50px 0 0 0;
+            padding-top: calc(50px + env(safe-area-inset-top))
+        }
+
+        .topbar {
+            border-top: none;
+            border-bottom: 1px solid goldenrod;
+            top: 0;
+            bottom: auto;
+        }
     }
 </style>

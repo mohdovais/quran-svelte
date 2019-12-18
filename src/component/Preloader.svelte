b71feb13f1cd2bf8f40495dcce1b6a319903142e
Mohd Ovais <ovais.m@gmail.com>
2019-12-18 23:35:42 +0100

    glyph

--- src/component/Preloader.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import IconAya from './IconAya.svelte';
-
-const style = "left: 50%;top: 50%;position: fixed;-webkit-transform: translate(-50%, -50%);transform: translate(-50%, -50%);width: 240px;height: 310px;"
-</script>
-
-<IconAya style="{style}" />
